e5a3000d7d0b96644875c9d64676453395a797be
Manuel <wtfjoke@livenet.ch>
2017-11-09 21:31:49 +0100

    extract current logic into separate method

--- scraper.py
+++ scraper.py
@@ -2,16 +2,26 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup, SoupStrainer
 
 
-url = "http://wtz-tagungszentrum.de/restaurants/"
-page = urlopen(url)
+URL = "http://wtz-tagungszentrum.de/restaurants/"
 
-if page.status != 200:
-    print('something wrong with url ' + url)
 
-soup = BeautifulSoup(page,  "html.parser", parse_only=SoupStrainer('a'))
-for link in soup:
-    if link.has_attr('href'):
-        target = link['href']
-        if target.endswith('.pdf'):
-            menu_card_link = target
-            print(menu_card_link)
+def extract_menu_card_link():
+    page = urlopen(URL)
+
+    if page.status != 200:
+        print('something wrong with url ' + URL)
+
+    soup = BeautifulSoup(page,  "html.parser", parse_only=SoupStrainer('a'))
+    menu_card_link = ''
+    for link in soup:
+        if link.has_attr('href'):
+            target = link['href']
+            if target.endswith('.pdf'):
+                menu_card_link = target
+
+    return menu_card_link
+
+
+# starter method
+if __name__ == "__main__":
+    print(extract_menu_card_link())
