808527cd28039aa995d505993d65c4648583bc15
Manuel <wtfjoke@livenet.ch>
2017-11-10 19:44:55 +0100

    add pdf text extraction

---
+++ pdf_extractor.py
@@ -0,0 +1,30 @@
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
+
+
+def convert_pdf_to_txt(path):
+    resource_manager = PDFResourceManager()
+    try:
+        with StringIO() as string_writer, open(path, 'rb') as pdf_file:
+            device = create_text_converter(resource_manager, string_writer)
+            interpreter = PDFPageInterpreter(resource_manager, device)
+
+            for page in PDFPage.get_pages(pdf_file, maxpages=1):
+                interpreter.process_page(page)
+
+            pdf_text = string_writer.getvalue()
+    finally:
+        device.close()
+    return pdf_text
+
+
+def create_text_converter(resource_manager, string_writer):
+    codec = 'utf-8'
+    return TextConverter(resource_manager, string_writer, codec=codec, laparams=LAParams())
+
+
+text = convert_pdf_to_txt('card.pdf')
+print(text)
