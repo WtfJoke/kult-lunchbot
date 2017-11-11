2f490193430d27a4075864b3c77426f79a8f1ca6
Manuel <wtfjoke@livenet.ch>
2017-11-11 21:57:50 +0100

    implement simple error handling on slack_sender

--- slack_sender.py
+++ slack_sender.py
@@ -1,13 +1,17 @@
 from slackclient import SlackClient
 import os
 
-slack_token = os.environ["SLACK_API_TOKEN"]
-sc = SlackClient(slack_token)
 
-response = sc.api_call(
-  "chat.postMessage",
-  channel="#lunch",
-  text="Hello from Lunchbot! :tada:"
-)
+def send_message(text):
+    slack_token = os.environ["SLACK_API_TOKEN"]
+    sc = SlackClient(slack_token)
 
-print(response)
+    response = sc.api_call(
+        "chat.postMessage",
+        channel="#lunch",
+        text=text)
+    if not response['ok']:
+        print("something bad happened: " + response['error'])
+
+
+send_message("Hello from Lunchbot! :tada:")
