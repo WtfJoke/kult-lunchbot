d267db7863e865f2bf44a64e32bf63394f9c819f
Manuel <wtfjoke@livenet.ch>
2017-11-11 16:26:08 +0100

    some fixes

--- lunchbot.py
+++ lunchbot.py
@@ -1,9 +1,15 @@
 import scraper
 import pdf_textractor
+from lunchmenu import Days
 
 
 # starter method
 if __name__ == "__main__":
     pdf = scraper.get_pdf()
-    text = pdf_textractor.get_menu_text(pdf)
-    print(text)
+    menu = pdf_textractor.get_menu(pdf)
+    print(str(menu.get_daily_menu(Days.THURSDAY)))
+    print(str(menu.get_daily_menu(Days.MONDAY).get_menu_two()))
+    print(str(menu))
+
+
+    #menus.get_items_for(monday).getMenu1()
