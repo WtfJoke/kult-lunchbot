a255d4b8ce046d03c927c9bf0cdb80f87e3b4c4f
Manuel <wtfjoke@livenet.ch>
2017-11-12 14:08:40 +0100

    add basic slack bot

--- bot.py
+++ bot.py
@@ -1,11 +1,92 @@
-from __future__ import print_function
-import recastai
+# -*- coding: utf-8 -*-
+# The MIT License (MIT)
+#
+# Copyright (c) 2016 Shannon Burns
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""
+Python Slack Bot class for use with the kult lunchbot app
+"""
 import os
 
-client = recastai.Client(os.environ["RECAST_TOKEN"], 'de')
-response = client.request.analyse_text('hallo')
+from slackclient import SlackClient
 
-if response.intent:
-  print(response.intent.slug)
-if response.intent.slug == 'YOUR_EXPECTED_INTENT':
-  """Do your code..."""
+# To remember which teams have authorized your app and what tokens are
+# associated with each team, we can store this information in memory on
+# as a global object. When your bot is out of development, it's best to
+# save this in a more persistant memory store.
+authed_teams = {}
+
+
+class Bot(object):
+    def __init__(self):
+        super(Bot, self).__init__()
+        self.name = "Lunchbot"
+        self.emoji = ":robot_face:"
+        # When we instantiate a new bot object, we can access the app
+        # credentials we set earlier in our local development environment.
+        self.oauth = {"client_id": os.environ.get("CLIENT_ID"),
+                      "client_secret": os.environ.get("CLIENT_SECRET"),
+                      # Scopes provide and limit permissions to what our app
+                      # can access. It's important to use the most restricted
+                      # scope that your app will need.
+                      "scope": "bot"}
+        self.verification = os.environ.get("VERIFICATION_TOKEN")
+
+        # NOTE: Python-slack requires a client connection to generate
+        # an oauth token. We can connect to the client without authenticating
+        # by passing an empty string as a token and then reinstantiating the
+        # client with a valid OAuth token once we have one.
+        self.client = SlackClient("")
+        # We'll use this dictionary to store the state of each message object.
+        # In a production envrionment you'll likely want to store this more
+        # persistantly in  a database.
+        self.messages = {}
+
+    def auth(self, code):
+        """
+        Authenticate with OAuth and assign correct scopes.
+        Save a dictionary of authed team information in memory on the bot
+        object.
+
+        Parameters
+        ----------
+        code : str
+            temporary authorization code sent by Slack to be exchanged for an
+            OAuth token
+
+        """
+        # After the user has authorized this app for use in their Slack team,
+        # Slack returns a temporary authorization code that we'll exchange for
+        # an OAuth token using the oauth.access endpoint
+        auth_response = self.client.api_call(
+                                "oauth.access",
+                                client_id=self.oauth["client_id"],
+                                client_secret=self.oauth["client_secret"],
+                                code=code
+                                )
+        # To keep track of authorized teams and their associated OAuth tokens,
+        # we will save the team ID and bot tokens to the global
+        # authed_teams object
+        team_id = auth_response["team_id"]
+        authed_teams[team_id] = {"bot_token":
+                                 auth_response["bot"]["bot_access_token"]}
+        # Then we'll reconnect to the Slack Client with the correct team's
+        # bot token
+        self.client = SlackClient(authed_teams[team_id]["bot_token"])
