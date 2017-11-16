a00707f7ef527f81a309d61f89ebddb258496b67
Manuel <wtfjoke@livenet.ch>
2017-11-16 18:23:21 +0100

    only crawl link when its necessary (also add logging)

--- scraper.py
+++ scraper.py
@@ -2,19 +2,26 @@ from urllib.request import urlopen, urlretrieve, Request
 from bs4 import BeautifulSoup, SoupStrainer
 import datetime
 import os
+import logging
 
 URL = "http://wtz-tagungszentrum.de/restaurants/"
 
 
 def get_pdf():
-    return download_pdf(extract_menu_card_link())
+    file = get_menu_file()
+    if not os.path.exists(file):
+        download_pdf(file)
+
+    return get_menu_file_name()
 
 
 def extract_menu_card_link():
+    logging.info("Crawl pdf")
     r = Request(URL, headers={'User-Agent': "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0"})
     page = urlopen(r)
     if page.status != 200:
         print('something wrong with url ' + URL)
+        logging.error("Something wrong with url: " + URL)
 
     soup = BeautifulSoup(page,  "html.parser", parse_only=SoupStrainer('a'))
     menu_card_link = ''
@@ -23,18 +30,17 @@ def extract_menu_card_link():
             target = link['href']
             if target.endswith('.pdf'):
                 menu_card_link = target
+                logging.info("Found pdf link: " + menu_card_link)
+                break
 
     return menu_card_link
 
 
-def download_pdf(link_to_pdf):
-    filename = get_menu_file_name()
-    file = get_menu_file()
-    if not os.path.exists(file):
-        print('Downloading pdf from ' + link_to_pdf)
-        result = urlretrieve(link_to_pdf, file)
-        # TODO: Better exception handling
-    return filename
+def download_pdf(file):
+    link_to_pdf = extract_menu_card_link()
+    logging.info("Download pdf from " + link_to_pdf)
+    result = urlretrieve(link_to_pdf, file)
+    # TODO: Better exception handling
 
 
 def get_menu_file_name():
@@ -55,4 +61,5 @@ def get_monday_date():
 
 # starter method
 if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.DEBUG)
     get_pdf()
