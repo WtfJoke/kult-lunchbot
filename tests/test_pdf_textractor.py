4d5623213d5da0d39bf1e679c80ed51ad32b4ac7
Manuel <wtfjoke@livenet.ch>
2017-12-03 13:50:30 +0100

    extend tests for another weekly day

--- tests/test_pdf_textractor.py
+++ tests/test_pdf_textractor.py
@@ -19,4 +19,24 @@ class PdfTextTractorTestCase(unittest.TestCase):
         expected_monday_menu_text = "Kl. Salat | Hähnchenbrust, dazu Gemüse & Wedges | Dessert"
         self.assertEqual("Montag", daily_menu.get_weekday())
         self.assertEqual("06.11.2017", daily_menu.get_date())
-        self.assertEqual(expected_monday_menu_text, daily_menu.get_menu_items()[0].get_menu_content())
+        menus = daily_menu.get_menu_items()
+        self.assertEqual(expected_monday_menu_text, menus[0].get_menu_content())
+        self.assertEqual(1, menus[0].get_menu_number())
+        self.assertEqual("Kl. Salat | Gratinierter Fisch mit Reis", menus[1].get_menu_content())
+        self.assertEqual(2, menus[1].get_menu_number())
+        self.assertEqual("Penne Arrabiata (vegetarisch)", menus[2].get_menu_content())
+        self.assertEqual(3, menus[2].get_menu_number())
+
+    def test_get_menu_tuesday_content_with_example(self):
+        menu = pdf_textractor.get_menu_from_pdf(self.example_pdf)
+        daily_menu = menu.get_daily_menus()[1]
+        expected_monday_menu_text = "Kl. Salat | Fischfilet mit Kräuterhaube und Duftreis | Dessert"
+        self.assertEqual("Dienstag", daily_menu.get_weekday())
+        self.assertEqual("07.11.2017", daily_menu.get_date())
+        menus = daily_menu.get_menu_items()
+        self.assertEqual(expected_monday_menu_text, menus[0].get_menu_content())
+        self.assertEqual(1, menus[0].get_menu_number())
+        self.assertEqual("Kl. Salat | Currywurst mit Pommes", menus[1].get_menu_content())
+        self.assertEqual(2, menus[1].get_menu_number())
+        self.assertEqual("Spätzlepfanne mit Marktgemüse (vegetarisch)", menus[2].get_menu_content())
+        self.assertEqual(3, menus[2].get_menu_number())
