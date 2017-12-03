2d12fa00d53a4058767996e2810134864822e306
Manuel <wtfjoke@livenet.ch>
2017-12-03 19:22:46 +0100

    extract some logic into separate methods to improve readability

--- configuration.py
+++ configuration.py
@@ -13,33 +13,39 @@ class Config:
         filename = "config.yml"
 
         if os.path.exists(filename):
-            with open(filename, 'r') as yml_file:
-                cfg = yaml.load(yml_file)
-            db_config = cfg['postgres']
-
-            host = db_config['host']
-            port = str(db_config['port'])
-            user = db_config['user']
-            password = db_config['password']
-            db = db_config['db_name']
-
-            # load config into environment variable
-            os.environ['RDS_HOSTNAME'] = host
-            os.environ['RDS_PORT'] = port
-            os.environ['RDS_DB_NAME'] = db
-            os.environ['RDS_USERNAME'] = user
-            os.environ['RDS_PASSWORD'] = password
-            logging.info("Set environment-variables successfully")
+            Config.load_from_file(filename)
         else:
-            logging.warning("Cant load config, assume variables get passed by environment variables")
-            if not Config.get_db_host_name() \
-                    or not Config.get_db_port() or not Config.get_db_name() \
-                    or not Config.get_db_user() or not Config.get_db_password():
-                message = "Please set all db configs"
-                print("Error: " + message)
-                raise Exception(message)
+            Config.load_from_environment()
 
         Config.is_config_initialized = True
+
+    @staticmethod
+    def load_from_environment():
+        logging.warning("Cant load config, assume variables get passed by environment variables")
+        if not Config.get_db_host_name() \
+                or not Config.get_db_port() or not Config.get_db_name() \
+                or not Config.get_db_user() or not Config.get_db_password():
+            message = "Please set all db configs"
+            print("Error: " + message)
+            raise Exception(message)
+
+    @staticmethod
+    def load_from_file(filename):
+        with open(filename, 'r') as yml_file:
+            cfg = yaml.load(yml_file)
+        db_config = cfg['postgres']
+        host = db_config['host']
+        port = str(db_config['port'])
+        user = db_config['user']
+        password = db_config['password']
+        db = db_config['db_name']
+        # load config into environment variable
+        os.environ['RDS_HOSTNAME'] = host
+        os.environ['RDS_PORT'] = port
+        os.environ['RDS_DB_NAME'] = db
+        os.environ['RDS_USERNAME'] = user
+        os.environ['RDS_PASSWORD'] = password
+        logging.info("Set environment-variables successfully")
 
     @staticmethod
     def get_db_host_name():
